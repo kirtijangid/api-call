8c88b20394e702e85bbe565df284c228685bbb3e
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-10-13 04:42:24 +0530

    creating list view builder

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,17 +11,29 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<dynamic> users = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('api call'),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: fetchUsers),
+      body: ListView.builder(itemBuilder: (context, index) {
+        return ListTile();
+      }),
+      floatingActionButton: FloatingActionButton(onPressed: fetchUsers),
     );
   }
 
-  void fetchUsers() {
+  void fetchUsers() async {
+    const url = 'https://randomuser.me/api/?results=5';
+    final uri = Uri.parse(url);
+    var http;
+    final response = await http.get(url);
+    final body = response.body;
+    final json = jsonDecode(body);
+    setState(() {
+      users = json['results'];
+    });
   }
 }
